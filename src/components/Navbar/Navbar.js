2884bb92b07d0d2266f0b55d15148863541562d7
João Henrique <joaohnsousa@gmail.com>
2023-07-04 21:43:41 -0300

    User Data e Login

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,40 +1,69 @@
-import Container from 'react-bootstrap/Container';
+import Container from "react-bootstrap/Container";
 import logo from "../../Imagens/logo-nav.png";
 import { Navbar, Nav } from "react-bootstrap";
-import { FaUserAlt } from 'react-icons/fa';
-import '../Styles/NavBar.css'
-import { Link } from 'react-router-dom';
+import { FaUserAlt } from "react-icons/fa";
+import "../Styles/NavBar.css";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../useContext/UserContext";
+import { useNavigate } from "react-router-dom";
 
 function NavBar() {
+  const [userData, setUserData] = useContext(UserContext);
+  const navigate = useNavigate();
+
+  async function logoutHandler(e) {
+    localStorage.setItem("email_Prosen", "");
+    localStorage.setItem("name_Prosen", "");
+    localStorage.setItem("id_Prosen", "");
+    localStorage.removeItem("logado_Prosen");
+    await navigate("/");
+    window.location.reload(true);
+    e.preventDefault();
+  }
+
   return (
-    <Navbar  expand="lg" className="nav-edit">
+    <Navbar expand="lg" className="nav-edit">
       <Container>
         <Navbar.Brand href="#home">
-          <Link to={'/'}>
-            <img src={logo} alt="Logo" id='logo-nav' />
+          <Link to={"/"}>
+            <img src={logo} alt="Logo" id="logo-nav" />
           </Link>
         </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Nav className="ml-auto">
-            
             <Nav.Link id="link-nav" href="/#Eventos">
               Eventos
             </Nav.Link>
             <Nav.Link id="link-nav" href="/#Repositorio">
               Repositório
             </Nav.Link>
-            <Nav.Link id="link-nav" target='blank' href="https://www.tecnicosenai.com.br/unidade-feira-de-santana/">
+            <Nav.Link
+              id="link-nav"
+              target="blank"
+              href="https://www.tecnicosenai.com.br/unidade-feira-de-santana/"
+            >
               Contato
             </Nav.Link>
-            <div className='link-nav-login'>
-              <Link to={'/dashboard'}>
-                <Nav.Link id="link-nav" href="#link">
-              Conta ProSen
-            </Nav.Link>
-              </Link>
-            
-            <FaUserAlt id='icon-nav'/>
+            <div className="link-nav-login">
+              {userData.logado ? (
+                <Link to={"/dashboard"}>
+                  <Nav.Link id="link-nav" href="#link">
+                    Bem vindo {userData.name}
+                  </Nav.Link>
+                </Link>
+              ) : (
+                <Link to={"/dashboard"}>
+                  <Nav.Link id="link-nav" href="#link">
+                    Conta ProSen
+                  </Nav.Link>
+                </Link>
+              )}
+              
+
+              <FaUserAlt id="icon-nav" />
+              <button onClick={logoutHandler}>sair</button>
             </div>
           </Nav>
         </Navbar.Collapse>
--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -1,54 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../../Navbar/Navbar";
-import "../../Styles/Login.css"
+import "../../Styles/Login.css";
 import BannerLogin from "../../Section-Banner/BannerLogin";
-import imglogologin from "../../../Imagens/logo-login.png"
-import {AiFillLock} from "react-icons/ai"
-import {FaUserAlt} from "react-icons/fa"
-import logoForm from "../../../Imagens/logo-form.png"
-import { Link } from "react-router-dom";
+import imglogologin from "../../../Imagens/logo-login.png";
+import { AiFillLock } from "react-icons/ai";
+import { FaUserAlt } from "react-icons/fa";
+import logoForm from "../../../Imagens/logo-form.png";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../../../services/api";
+import { useContext } from "react";
+import { UserContext } from "../../useContext/UserContext";
 
-function Login(){
-    return(
-        <>
-        <NavBar/>
-        <BannerLogin/>
-        <div className="login-sec">
+function Login() {
+  const [UserData, setUserData] = useContext(UserContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [nes, setNes] = useState([]);
+  const navigate = useNavigate();
+
+
+  const logar = async (e) => {
+    e.preventDefault();
+    try{
+      const Data = await api.post("auth/login", {
+        email,
+        password
+      });
+      console.log(Data.data);
+      const tokenGuard = JSON.stringify(Data.data.token).replace(/["]/g, "");
+      const id = JSON.stringify(Data.data.usuario._id).replace(/["]/g, "");
+      const nome = JSON.stringify(Data.data.usuario.name).replace(/["]/g, "");
+      const emailProsen = JSON.stringify(Data.data.usuario.email).replace(/["]/g, "");
+
+      localStorage.setItem("token_Prosen", tokenGuard);
+      localStorage.setItem("id_Prosen", id);
+      localStorage.setItem("name_Prosen", nome);
+      localStorage.setItem("email_Prosen", emailProsen);
+      localStorage.setItem("logado_Prosen", true);
+      await navigate("/");
+      window.location.reload(true);
+    }catch{
+      console.log("Não funcionou");
+    }
+  };
+
+  return (
+    <>
+      <NavBar />
+      <BannerLogin />
+      <div className="login-sec">
         <div className="div-img-form">
-            <img alt=""  src={imglogologin}/>
-                </div>
-                <div>
-           
+          <img alt="" src={imglogologin} />
+        </div>
+        <div>
           <div class="center-edit">
-                    <div className="div-form-text">
-                        <h3>Conta ProSen</h3>
-                        <img alt="" id="logo-form" src={logoForm}/>
-                    </div>
-                          <form>
-                                <div class="inputbox">
-                                    <input type="text" placeholder="Email" required="required" />
-                                    <i class="icon"><FaUserAlt id="icon-form-user"/></i>
-                                    
-                                </div>
-                                <div class="inputbox">
-                                    <input type="text" placeholder="Senha" required="required" />
-                                    <i class="icon"><AiFillLock id="icon-form" /></i>
-                                   
-                                </div>
-                                <Link to="dashboard">
-                                <div class="div-button">
-                                    <button class="btn-form" type="submit">ACESSAR</button>
-                                </div>
-                                </Link>
-                    </form>
-                    </div>
-                </div>
-                </div>
-            
-        </>
+            <div className="div-form-text">
+              <h3>Conta ProSen</h3>
+              <img alt="" id="logo-form" src={logoForm} />
+            </div>
+            <form>
+              <div class="inputbox">
+                <input
+                  type="text"
+                  placeholder="Email"
+                  required="required"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                />
+                <i class="icon">
+                  <FaUserAlt id="icon-form-user" />
+                </i>
+              </div>
+              <div class="inputbox">
+                <input
+                  type="text"
+                  placeholder="Senha"
+                  required="required"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                />
+                <i class="icon">
+                  <AiFillLock id="icon-form" />
+                </i>
+              </div>
 
-    )
+              <div class="div-button" onClick={logar}>
+                <button class="btn-form">
+                  ACESSAR
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-
-export default Login
+export default Login;
